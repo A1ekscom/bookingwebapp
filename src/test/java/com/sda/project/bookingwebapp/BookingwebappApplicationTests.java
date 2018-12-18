b630b43369ce522e192a4ebf36f0d382f1d76323
Aleks Kosiak <a1ekskosyak@gmail.com>
2018-12-18 20:06:06 +0200

    Initial commit

---
+++ src/test/java/com/sda/project/bookingwebapp/BookingwebappApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sda.project.bookingwebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookingwebappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
