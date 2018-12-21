15669974334c0a26e1c4270c1d5648f7dc84dbbc
Aleks Kosiak <a1ekskosyak@gmail.com>
2018-12-21 22:08:29 +0200

    Initial front-end implementation

---
+++ src/main/java/com/sda/project/bookingwebapp/controller/NewsletterController.java
@@ -0,0 +1,30 @@
+package com.sda.project.bookingwebapp.controller;
+
+import com.sda.project.bookingwebapp.model.NewsletterModel;
+import com.sda.project.bookingwebapp.service.NewsletterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class NewsletterController {
+
+    @Autowired
+    private NewsletterService newsletterService;
+
+    @GetMapping("/newsletter")
+    public ModelAndView indexPage() {
+        return new ModelAndView("index")
+                .addObject("newsletter", new NewsletterModel())
+                .addObject("user", "Aleks");
+    }
+
+    @PostMapping("/newsletter")
+    public ModelAndView insertNewsletter(@ModelAttribute("email") final String email) {
+        newsletterService.insertNewsletter(email);
+        return new ModelAndView("result").addObject("email", email);
+    }
+}
